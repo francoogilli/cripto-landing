a26bbe0a929a2cbf4fae1e3db4724ad469cfeed0
Franco Gilli <francogilli10@gmail.com>
2024-03-10 16:53:36 -0300

    Add popular NFT sliders in Trending component

---
+++ src/data.tsx
@@ -0,0 +1,28 @@
+
+
+export const CardsData = [
+    {
+        image: 'image.png',
+        name: 'Hacker',
+        value:'2.432,21$',
+        eth: '1.2'
+    },
+    {
+        image: 'corgi.png',
+        name: 'Rober',
+        value:'1.432,21$',
+        eth: '0.7'
+    },
+    {
+        image: 'perro.jpg',
+        name: 'Hacker',
+        value:'2.432,21$',
+        eth: '1.2'
+    },
+    {
+        image: 'tiger.jpeg',
+        name: 'Hacker',
+        value:'2.432,21$',
+        eth: '1.2'
+    }
+  ]
