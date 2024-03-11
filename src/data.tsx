85facf709af5aa676ad28dc9c3b7f171dd2332f7
Franco Gilli <francogilli10@gmail.com>
2024-03-10 23:14:01 -0300

    Add new avatars icons

--- src/data.tsx
+++ src/data.tsx
@@ -3,24 +3,28 @@
 export const CardsData = [
     {
         image: '/images/image.png',
+        avatar: '/avatars/Avatar.svg',
         name: 'Hacker',
         value:'2.432,21$',
         eth: '1.2'
     },
     {
         image: '/images/corgi.png',
+        avatar: '/avatars/Avatar1.svg',
         name: 'Rober',
         value:'1.432,21$',
         eth: '0.7'
     },
     {
         image: '/images/perro.jpg',
+        avatar: '/avatars/Avatar2.svg',
         name: 'Hacker',
         value:'2.432,21$',
         eth: '1.2'
     },
     {
         image: '/images/tiger.jpeg',
+        avatar: '/avatars/Avatar3.svg',
         name: 'Hacker',
         value:'2.432,21$',
         eth: '1.2'
@@ -31,26 +35,51 @@ export const CardsData = [
   export const CollectorsData = [
     {
         username: '@johnNFT',
+        avatar: '/avatars/Avatar.svg',
         name: 'Hacker',
         percentage:'2.432,21',
         eth: '20'
     },
     {
-        username: '@robertNFT',
+        username: '@robert',
+        avatar: '/avatars/Avatar1.svg',
         name: 'Rober',
         percentage:'1.432,21',
         eth: '20'
     },
     {
         username: '@franco',
+        avatar: '/avatars/Avatar3.svg',
         name: 'Franco',
-        percentage:'2.432,21',
+        percentage:'5.567,12',
         eth: '20'
     },
     {
         username: '@elisaNFT',
+        avatar: '/avatars/Avatar2.svg',
         name: 'Elisa',
         percentage:'2.432,21',
         eth: '20'
+    },
+    {
+        username: '@david',
+        avatar: '/avatars/Avatar4.svg',
+        name: 'David',
+        percentage:'1.431,21',
+        eth: '19'
+    },
+    {
+        username: '@melissa',
+        avatar: '/avatars/Avatar5.svg',
+        name: 'Melissa',
+        percentage:'1.332,25',
+        eth: '31'
+    },
+    {
+        username: '@marilyn',
+        avatar: '/avatars/Avatar6.svg',
+        name: 'Marilyn',
+        percentage:'4.120,98',
+        eth: '52'
     }
   ]
