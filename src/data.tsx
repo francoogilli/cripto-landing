65e6fb2155e1d40097362fefaa77ddf96b77b790
Franco Gilli <francogilli10@gmail.com>
2024-03-11 08:39:56 -0300

    Change color in Collectors percentage

--- src/data.tsx
+++ src/data.tsx
@@ -38,48 +38,55 @@ export const CardsData = [
         avatar: '/avatars/Avatar.svg',
         name: 'Hacker',
         percentage:'2.432,21',
-        eth: '20'
+        eth: '20',
+        color:'#EF4870'
     },
     {
         username: '@robert',
         avatar: '/avatars/Avatar1.svg',
         name: 'Rober',
         percentage:'1.432,21',
-        eth: '20'
+        eth: '20',
+        color:'#4BCE97'
     },
     {
         username: '@franco',
         avatar: '/avatars/Avatar3.svg',
         name: 'Franco',
         percentage:'5.567,12',
-        eth: '20'
+        eth: '20',
+        color:'#EF4870'
     },
     {
         username: '@elisaNFT',
         avatar: '/avatars/Avatar2.svg',
         name: 'Elisa',
         percentage:'2.432,21',
-        eth: '20'
+        eth: '20',
+        color:'#4BCE97'
     },
     {
         username: '@david',
         avatar: '/avatars/Avatar4.svg',
         name: 'David',
         percentage:'1.431,21',
-        eth: '19'
+        eth: '19',
+        color:'#4BCE97'
     },
     {
         username: '@melissa',
         avatar: '/avatars/Avatar5.svg',
         name: 'Melissa',
         percentage:'1.332,25',
-        eth: '31'
+        eth: '31',
+        color:'#EF4870'
     },
     {
         username: '@marilyn',
         avatar: '/avatars/Avatar6.svg',
         name: 'Marilyn',
         percentage:'4.120,98',
-        eth: '52'
+        eth: '52',
+        color:'#EF4870'
     }
   ]
