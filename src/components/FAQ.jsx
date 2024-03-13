e3087664fdbd4784f6f8820e750dfdacacc0ac49
Franco Gilli <francogilli10@gmail.com>
2024-03-13 12:36:50 -0300

    Improve font-weight in questions

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -18,7 +18,7 @@ const FAQ = () => {
             <div className="divide-y divide-zinc-800">
                 {QuestionsData.map(({ question, answer }, index) => (
                     <div className={`accordion-item text-white duration-300 rounded-lg ${openItems[index] ? 'bg-[#131313]' : 'hover:bg-[#131313]'}`} key={index}>
-                        <div className="flex justify-between px-4 py-5 text-base md:text-lg font-medium cursor-pointer" onClick={() => toggleAccordion(index)}>
+                        <div className="flex justify-between px-4 py-5 text-base md:text-lg font-semibold cursor-pointer" onClick={() => toggleAccordion(index)}>
                             {question}
                             <svg xmlns="http://www.w3.org/2000/svg" className="border border-white/10 rounded-full p-1" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 {!openItems[index] ? <path className="line" d="M12 5l0 14" /> : <path className="line" d="M5 12h14" />}
