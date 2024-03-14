73c9568d1605304a30bde42671be7a6a9055e38e
Franco Gilli <francogilli10@gmail.com>
2024-03-14 17:28:20 -0300

    Add numbers next to questions

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -18,8 +18,11 @@ const FAQ = () => {
             <div className="divide-y divide-[#1F1B26]">
                 {QuestionsData.map(({ question, answer }, index) => (
                     <div className={`accordion-item text-white duration-300 rounded-lg ${openItems[index] ? 'bg-[#131313]' : 'hover:bg-[#131313]'}`} key={index}>
-                        <div className="flex justify-between items-center px-4 py-5 text-base md:text-lg  font-semibold cursor-pointer" onClick={() => toggleAccordion(index)}>
-                            {question}
+                        <div className="flex justify-between items-center px-4 py-5 font-semibold cursor-pointer" onClick={() => toggleAccordion(index)}>
+                            <div className='flex justify-center items-center gap-x-4'>
+                                <p className="bg-[#7F35FF] flex justify-center font-bold md:text-sm text-xs items-center text-white size-5 md:size-6 px-2 rounded-md">{index + 1}</p>
+                                <p className='text-base md:text-lg'>{question}</p>
+                            </div>
                             <svg xmlns="http://www.w3.org/2000/svg" className={`  rounded-full p-1 size-5 md:size-6 ${openItems[index] ? 'bg-zinc-700' : 'bg-[#7F35FF]'}`} width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 {!openItems[index] ? <path className="line" d="M12 5l0 14" /> : <path className="line" d="M5 12h14" />}
                                 {!openItems[index] ? <path className="plus" d="M12 5v14M5 12h14" /> : null}
