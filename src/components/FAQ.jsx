2a4a12146412502f92a1ed3c920fc5cf71975765
Franco Gilli <francogilli10@gmail.com>
2024-03-13 23:00:44 -0300

    Change color to hide answer button in FAQ

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -20,7 +20,7 @@ const FAQ = () => {
                     <div className={`accordion-item text-white duration-300 rounded-lg ${openItems[index] ? 'bg-[#131313]' : 'hover:bg-[#131313]'}`} key={index}>
                         <div className="flex justify-between items-center px-4 py-5 text-base md:text-lg  font-semibold cursor-pointer" onClick={() => toggleAccordion(index)}>
                             {question}
-                            <svg xmlns="http://www.w3.org/2000/svg" className=" bg-[#7F35FF] rounded-full p-1 size-5 md:size-7" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" className={`  rounded-full p-1 size-5 md:size-7 ${openItems[index] ? 'bg-zinc-700' : 'bg-[#7F35FF]'}`} width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 {!openItems[index] ? <path className="line" d="M12 5l0 14" /> : <path className="line" d="M5 12h14" />}
                                 {!openItems[index] ? <path className="plus" d="M12 5v14M5 12h14" /> : null}
                             </svg>
